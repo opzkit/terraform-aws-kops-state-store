df40071498380ee36b79b29611beeb7ca90d1b22
Peter Svensson <peter@sparetimecoders.com>
2022-10-24 09:17:28 +0200

    feat: support KMS key for encryption at rest

--- main.tf
+++ main.tf
@@ -30,7 +30,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state_store" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = var.kms_key
+      sse_algorithm     = "aws:kms"
     }
   }
 }
